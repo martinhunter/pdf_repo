a4b1efca3b28ef40d30fbb072b181342c0d433d9
Martin <44609183+martinhunter@users.noreply.github.com>
2023-09-06 00:47:05 +0800

    add ply

--- lttng_cmds.sh
+++ lttng_cmds.sh
@@ -6,7 +6,8 @@
 # $1 is the name of tracing, any name is ok
 lttng create $1
 lttng enable-event -k --syscall --all
-lttng enable-event -k sched_switch,sched_wak'*',irq_'*',net_'*',sock_'*',skb_'*',napi_poll
+lttng enable-event -k sched_switch,sched_wak'*',irq_'*',net_'*',sock_'*',skb_'*',napi_poll,lttng_statedump_'*'
+#lttng enable-event -k --probe=net_rx_action my_net_rx_action
 lttng add-context -k -t vtid -t vpid -t procname -t prio
 lttng start
 sleep 10
